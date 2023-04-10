5ed923bc7decf34902a1d985bb21546a0f3fbf9d
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-04-11 00:07:29 +0200

    Deleted files and started navBar styling

---
+++ src/HomeScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCclhfotp2ie3Y0L9z4FJ3w0z84aWieCi8',
+  authDomain: 'movie-app-9999d.firebaseapp.com',
+  projectId: 'movie-app-9999d',
+  storageBucket: 'movie-app-9999d.appspot.com',
+  messagingSenderId: '163828624196',
+  appId: '1:163828624196:web:07325c791deb6d751e0511',
+};
